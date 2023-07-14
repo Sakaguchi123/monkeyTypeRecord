967b3e168dc3552aba40a9c615ee2e35d3590e5b
Sakaguchi123 <133064214+Sakaguchi123@users.noreply.github.com>
2023-07-14 17:20:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,46 @@ flatpickr('#flatpickr', {
   dateFormat: 'Y/m/d H:i',
   allowInput: true,
 });
-console.log("ringo")
-console.log("main")//vscode-app/c:/Users/chiri/AppData/Local/Programs/Microsoft%20VS%20Code/resources/app/out/vs/code/electron-sandbox/workbench/workbench.html
+
+let inputDate = document.querySelector("#flatpickr");
+let inputWpm = document.querySelector("#input-wpm");
+let selectTime = document.querySelector("#select-time");
+let submitButton = document.querySelector("#submit-button");
+
+let dateArr = [];
+let wpmArr = [];
+let timeArr = [];
+
+//wpmに入力されていないときはclick出来ないようにする。
+inputWpm.addEventListener("input", function() {
+  if (inputWpm.value === "") {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
+  }
+});
+
+//clickされた時の処理
+submitButton.addEventListener("click",function() {
+
+  if(localStorage.getItem("submitDate") === null) {
+    dateArr.push(inputDate.value);
+    wpmArr.push(inputWpm.value);
+    timeArr.push(selectTime.value);
+    localStorage.setItem("submitDate", JSON.stringify(dateArr));
+    localStorage.setItem("submitWpm", JSON.stringify(wpmArr));
+    localStorage.setItem("submitTime", JSON.stringify(timeArr));
+  }
+})
+//値がなかったら、null　もしあったら、
+
+
+// inputDate.addEventListener("change", change);
+// inputWpm.addEventListener("change", change);
+// selectTime.addEventListener("change", change);
+
+// function change() {
+//   console.log(selectTime.value);
+//   console.log(inputDate.value);
+//   console.log(inputWpm.value);
+// }
