9a712e82e6bcb28b963350293daa39a5e5115ac9
chirichiri123 <nn910304@gmail.com>
2023-07-16 21:52:07 +0900

    編集した

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ flatpickr('#flatpickr', {
   allowInput: true,
 });
 
+// localStorage.clear();
+
 const inputDate = document.querySelector("#flatpickr");
 const inputWpm = document.querySelector("#input-wpm");
 const selectTime = document.querySelector("#select-time");
 const submitButton = document.querySelector("#submit-button");
 const ctx = document.getElementById("chart");
+const list = document.querySelector("#record-list");
 
 let dateArr = [];
 let wpmArr = [];
@@ -37,31 +40,81 @@ submitButton.addEventListener("click",function() {
     localStorage.setItem("submitDate", JSON.stringify(dateArr));
     localStorage.setItem("submitWpm", JSON.stringify(wpmArr));
     localStorage.setItem("submitTime", JSON.stringify(timeArr));
+  }else {
+    let getDate = localStorage.getItem("submitDate");
+    let getWpm = localStorage.getItem("submitWpm");
+    let getTime = localStorage.getItem("submitTime");
+
+    getDate = JSON.parse(getDate);
+    getWpm = JSON.parse(getWpm);
+    getTime = JSON.parse(getTime);
+
+    getDate.push(inputDate.value);
+    getWpm.push(inputWpm.value);
+    getTime.push(selectTime.value);
+
+    localStorage.setItem("submitDate", JSON.stringify(getDate));
+    localStorage.setItem("submitWpm", JSON.stringify(getWpm));
+    localStorage.setItem("submitTime", JSON.stringify(getTime));
   }
+
+  inputWpm.value = ""; //ボタン押した後、wpmを空欄にする
 })
-//値がなかったら、null　もしあったら、
+
+//リスト表示
+let listGetDate = JSON.parse(localStorage.getItem("submitDate"));
+let listGetWpm = JSON.parse(localStorage.getItem("submitWpm"));
+let listGetTime = JSON.parse(localStorage.getItem("submitTime"));
+
+
+let i = 0;
+listGetDate.forEach((element) => {
+  const tr = document.createElement("tr");
+  list.appendChild(tr);
+
+  const tdDate = document.createElement("td");
+  tdDate.textContent = listGetDate[i];
+  const tdWpm = document.createElement("td");
+  tdWpm.textContent = listGetWpm[i];
+  const tdTime = document.createElement("td");
+  tdTime.textContent = listGetTime[i];
+
+  i++;
+
+  tr.appendChild(tdDate);
+  tr.appendChild(tdWpm);
+  tr.appendChild(tdTime);
+});
+
+
 
 
 let config  = new Chart(ctx, {
   type: "line",
   data: {
-    labels : [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-    ],
+    labels : JSON.parse(localStorage.getItem("submitDate")),
     datasets : [{
       label: "my first data set",
-      backgroundColor: 'rgb(255, 99, 132)',      
-      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: '#e2b714',      
+      borderColor: '#e2b714',
       data: [0, 10, 5, 2, 20, 30, 45],
     }]
   },
-  oputions:{}
+  options:{
+    responsive: true,
+    aspectRatio: 2,
+    plugins: {
+      legend: {
+        display: false, // 凡例を非表示
+      },
+      title: {
+        display: true,
+        text: "wpms",
+      },
+    }
+  }
 })
+
 
 
 
